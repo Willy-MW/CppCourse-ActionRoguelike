4e0afb8c6e38cd16bfad6bce0648494ec900369e
Marcin Witek <witek.marcin96@gmail.com>
2025-06-02 21:02:38 +0200

    Created AI character, controller, blackboard and behavior tree. Created simple follow logic and service to check if player is in attack range

---
+++ Source/ActionRoguelike/Private/AI/ARAIController.cpp
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "AI/ARAIController.h"
+#include "BehaviorTree/BlackboardComponent.h"
+#include "Kismet/GameplayStatics.h"
+
+void AARAIController::BeginPlay()
+{
+	Super::BeginPlay();
+
+	RunBehaviorTree(BehaviorTree);
+
+	APawn* MyPawn = UGameplayStatics::GetPlayerPawn(this, 0);
+	if (MyPawn)
+	{
+		GetBlackboardComponent()->SetValueAsVector(TEXT("Location"), MyPawn->GetActorLocation());
+		GetBlackboardComponent()->SetValueAsObject(TEXT("TargetActor"), MyPawn);
+	}
+}
---
+++ Source/ActionRoguelike/Private/AI/ARBTService_CheckAttackRange.cpp
@@ -0,0 +1,43 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "AI/ARBTService_CheckAttackRange.h"
+
+#include "AIController.h"
+#include "BehaviorTree/BlackboardComponent.h"
+
+void UARBTService_CheckAttackRange::TickNode(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory, float DeltaSeconds)
+{
+	Super::TickNode(OwnerComp, NodeMemory, DeltaSeconds);
+
+	UBlackboardComponent* BlackboardComponent = OwnerComp.GetBlackboardComponent();
+
+	if (ensure(BlackboardComponent))
+	{
+		AActor* TargetActor = Cast<AActor>(BlackboardComponent->GetValueAsObject(TEXT("TargetActor")));
+
+		if (TargetActor)
+		{
+			AAIController* MyController = OwnerComp.GetAIOwner();
+
+			if (ensure(MyController))
+			{
+				APawn* AIPawn = MyController->GetPawn();
+				if (ensure(AIPawn))
+				{
+					float DistanceTo = FVector::Distance(TargetActor->GetActorLocation(), AIPawn->GetActorLocation());
+					bool bWithinRange = DistanceTo < 2000.f;
+
+					bool bHasLOS = false;
+					if (bWithinRange)
+					{
+						bHasLOS = MyController->LineOfSightTo(TargetActor);
+					}
+
+
+					BlackboardComponent->SetValueAsBool(AttackRangeKey.SelectedKeyName, (bWithinRange && bHasLOS));
+				}
+			}
+		}
+	}
+}
